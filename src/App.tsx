303efaedc54f2c60056a9a29113b685222d7961a
Mason Anders <mason.anders@gopuff.com>
2023-08-03 18:19:07 -0700

    Export Map to GoogleMap component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Loader } from "@googlemaps/js-api-loader";
+import GoogleMap from "./components/GoogleMap";
 
 export default function App() {
-  const [mapEl, setMapEl] = useState<HTMLElement | null>(null);
-
-  useEffect(() => {
-    if (mapEl) {
-      const loader = new Loader({
-        apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string,
-        version: "weekly",
-      });
-
-      loader.load().then(async () => {
-        const { Map }: google.maps.MapsLibrary =
-          (await google.maps.importLibrary("maps")) as google.maps.MapsLibrary;
-
-        new Map(mapEl, {
-          center: {
-            lat: -34.397,
-            lng: 150.644,
-          },
-          scrollwheel: false,
-          zoom: 8,
-        });
-      });
-    }
-  }, [mapEl]);
-
-  return <div ref={setMapEl} style={{ height: "100vh", width: "100vw" }} />;
+  return (
+    <div>
+      <GoogleMap />
+    </div>
+  );
 }
