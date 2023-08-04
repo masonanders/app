d12fff5226364a5933048ffb466826ebc58d3e94
Mason Anders <mason.anders@gopuff.com>
2023-08-03 18:13:23 -0700

    Load Map element

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
 
 export default function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+  const [mapEl, setMapEl] = useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    if (mapEl) {
+      const loader = new Loader({
+        apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string,
+        version: "weekly",
+      });
+
+      loader.load().then(async () => {
+        const { Map }: google.maps.MapsLibrary =
+          (await google.maps.importLibrary("maps")) as google.maps.MapsLibrary;
+
+        new Map(mapEl, {
+          center: {
+            lat: -34.397,
+            lng: 150.644,
+          },
+          scrollwheel: false,
+          zoom: 8,
+        });
+      });
+    }
+  }, [mapEl]);
+
+  return <div ref={setMapEl} style={{ height: "100vh", width: "100vw" }} />;
 }
