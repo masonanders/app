41dda8ff97fa47ca6c501cb4955cb7e4bb54ea18
Mason Anders <mason.anders@gopuff.com>
2023-08-04 12:49:05 -0700

    Extract Result parts to reusable components

---
+++ src/components/Result/index.tsx
@@ -0,0 +1,60 @@
+import styled, { StyledComponent } from "styled-components";
+import { ReactComponent as IconPinSVG } from "../../assets/svg/icon-pin.svg";
+import { ResultType } from "../Results/sample-data";
+
+export const ResultIconContainer = styled.div`
+  align-items: center;
+  display: flex;
+  grid-area: icon;
+  justify-content: center;
+
+  & svg {
+    fill: #5281f7;
+    height: 32px;
+    width: 32px;
+  }
+`;
+
+export function ResultIcon({
+  Container = ResultIconContainer,
+}: {
+  Container?: StyledComponent<any, any>;
+}) {
+  return (
+    <Container>
+      <IconPinSVG />
+    </Container>
+  );
+}
+
+export const ResultTitleContainer = styled.div`
+  font-weight: bold;
+  grid-area: title;
+`;
+
+export function ResultTitle({
+  Container = ResultTitleContainer,
+  result,
+}: {
+  Container?: StyledComponent<any, any>;
+  result: ResultType;
+}) {
+  return <Container>{result.name}</Container>;
+}
+
+export const ResultCoordsContainer = styled.div`
+  color: darkgray;
+  grid-area: coord;
+`;
+
+export function ResultCoords({
+  Container = ResultCoordsContainer,
+  result,
+}: {
+  Container?: StyledComponent<any, any>;
+  result: ResultType;
+}) {
+  return (
+    <Container>{`${result.location.lat}, ${result.location.lon}`}</Container>
+  );
+}
--- src/components/Results/Result.tsx
+++ src/components/Results/Result.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { ReactComponent as IconPinSVG } from "../../assets/svg/icon-pin.svg";
 import { ResultType } from "./sample-data";
+import { ResultCoords, ResultIcon, ResultTitle } from "../Result";
 
 const ResultContainer = styled.li`
   column-gap: 16px;
@@ -19,47 +19,17 @@ const ResultContainer = styled.li`
     background-color: #eeeefa;
   }
 `;
-
-const ResultIcon = styled.div`
-  align-items: center;
-  display: flex;
-  grid-area: icon;
-  justify-content: center;
-
-  & svg {
-    fill: #5281f7;
-    height: 32px;
-    width: 32px;
-  }
-`;
-
-const ResultTitle = styled.div`
-  font-weight: bold;
-  grid-area: title;
-`;
-
-const ResultCoord = styled.div`
-  color: darkgray;
-  grid-area: coord;
-`;
-
 type ResultProps = {
   onClick: (result: ResultType) => void;
   result: ResultType;
 };
 
 export default function Result({ onClick, result }: ResultProps) {
-  const {
-    name,
-    location: { lat, lon },
-  } = result;
   return (
     <ResultContainer onClick={() => onClick(result)}>
-      <ResultIcon>
-        <IconPinSVG />
-      </ResultIcon>
-      <ResultTitle>{name}</ResultTitle>
-      <ResultCoord>{`${lat}, ${lon}`}</ResultCoord>
+      <ResultIcon />
+      <ResultTitle result={result} />
+      <ResultCoords result={result} />
     </ResultContainer>
   );
 }
